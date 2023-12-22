568420e2ec83bf665588d720e7d26441f5853130
Chaitrali Shinde <chai.shinde100@gmail.com>
2023-12-22 14:42:25 +0530

    basic front-end of the ticketing app

---
+++ app/(components)/StatusDisplay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StatusDisplay = () => {
+  return (
+    <span className="inline-block rounded-full py-1 text-xs font-semibold text-grey-700 bg-green-400">
+      done
+    </span>
+  );
+};
+
+export default StatusDisplay;
