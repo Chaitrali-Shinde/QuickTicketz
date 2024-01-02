f0b2f6a1322e6a3c115c70cfe33d70a024c3d749
Chaitrali Shinde <chai.shinde100@gmail.com>
2024-01-02 20:25:05 +0530

    category wise display of tickets

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const StatusDisplay = () => {
+const StatusDisplay = ({ status }) => {
   return (
     <span className="inline-block rounded-full py-1 text-xs font-semibold text-grey-700 bg-green-400">
-      done
+      {status}
     </span>
   );
 };
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,48 @@
 import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get tickets", error);
+  }
+};
+
+const Dashboard = async () => {
+  // {} allows you to unpack values from arrays, or properties from objects, into distinct variables.
+  const { tickets } = await getTickets();
+
+  //remove dublicates
+  /// ... is called spread operator
+  //tickets?.map(...) means "if tickets is not null or undefined, then call map on it; otherwise, return undefined." This prevents a runtime error that would occur if you tried to call map on null or undefined.
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+  console.log(uniqueCategories);
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{uniqueCategory}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCategory)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
